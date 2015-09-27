a00333013853fed352847882958abb85fa9a0296
thynuss <college.biol@gmail.com>
2015-09-27 15:46:37 -0700

    Programming Assignment 2
    Rosa Runcie
    code for assignment number 2

--- cachematrix.R
+++ cachematrix.R
@@ -13,3 +13,48 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+### The two functions I use for this assignment are makeCacheMatrix(xm = matrix()) and cacheSolve(xm). 
+# The makeCacheMatrix function will set matrix, get matrix, set inverse, and get inverse.
+# Functions setM and getM are for setting and getting input matrix xm.  
+# Functions setInvM and getInvM are for setting and getting the calculated inverse matrix. 
+# The variable invM is a free variable in makeCacheMatrix, with assignment operator <<-
+
+#makeCacheMatrix
+makeCacheMatrix <- function(xm = matrix){
+  invM <- NULL
+  setM <- function(ym) {
+    print("In setM.")
+    ym
+    invM <<- NULL             
+    xm <<- ym
+    print("In setM")
+    xm
+  }
+  getM <- function() {
+    print("In getM.")
+    xm
+  }
+  setInvM <- function(im) {
+    print("In setInvM.")
+    invM <<- im
+  }
+  getInvM <- function() {
+    print("In invM.")
+    invM
+  }
+  list(setM = setM, getM = getM, setInvM = setInvM, getInvM = getInvM)
+}
+
+#solveCache
+cacheSolve <- function(x, ...) {
+  print("in cacheSolve, xm = ")
+  iM <- xm$getInvM()
+  if(!is.null(iM)) {
+    message("getting cached data")
+    return (iM)
+  }
+  data <- xm$getM()
+  iM <- solve(data, ...)
+  xm$setInvM(iM)
+  iM
+}
